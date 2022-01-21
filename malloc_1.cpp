bc38b0782d958491aef06d4a406dd1b83416a373
noam wolf <noamswolf@gmail.com>
2022-01-21 12:42:23 +0000

    smalloc

--- malloc_1.cpp
+++ malloc_1.cpp
@@ -1,4 +1,9 @@
-//
-// Created by student on 1/21/22.
-//
-
+#include <unistd.h>
+void* smalloc(size_t size)
+{
+    if(!size || size > 100000000
+    {
+        return NULL;
+    }
+    return sbrk(size);
+}
