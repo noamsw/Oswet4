0262894b9b3be291f5be3578babf6e2718bb1200
muradek <muradek@campus.technion.ac.il>
2022-01-23 21:33:11 +0000

    debugged malloc_2

--- malloc_2.cpp
+++ malloc_2.cpp
@@ -1,10 +1,11 @@
 #include <unistd.h>
 #include <cstring>
+#include <iostream>
 #include "malloc_2.h"
 
 struct MallocMetaData
 {
-    size_t size; // the size of the total allocation
+    size_t total_size; // the size of the total allocation
     bool is_free;
     MallocMetaData* next;
     MallocMetaData* prev;
@@ -34,11 +35,11 @@ void* smalloc(size_t size)
             first_node = NULL;
             return NULL;
         }
-        first_node->size = total_size;
+        first_node->total_size = total_size;
         first_node->is_free = false;
         first_node->next = NULL;
         first_node->prev = NULL;
-
+        // std::cout << "size of first alloc: " << first_node->total_size << std::endl;
         // allocating requested size
         void* block_ptr = sbrk(size);
         if (block_ptr == (void*)(-1))
@@ -58,7 +59,7 @@ void* smalloc(size_t size)
     MallocMetaData* last_node = first_node; // if we wont find a block, update last_node->next
     while (curr_node != NULL)
     {
-        if ((curr_node->size >= total_size) && curr_node->is_free)
+        if ((curr_node->total_size >= total_size) && curr_node->is_free)
             break;
         last_node = curr_node;
         curr_node = curr_node->next;
@@ -72,18 +73,19 @@ void* smalloc(size_t size)
         {
             return NULL;
         }
-        new_meta->size = total_size;
+        new_meta->total_size = total_size;
         new_meta->is_free = false;
         new_meta->next = NULL;
         new_meta->prev = last_node;
         last_node->next = new_meta;
+        // std::cout << "size of sec alloc: " << new_meta->total_size << std::endl;
 
         // alloc requested
         void* block_ptr = sbrk(size);
         if (block_ptr == (void*)(-1))
         {
             last_node->next = NULL;
-            sbrk((-sizeof(new_meta)));
+            sbrk((-sizeof(MallocMetaData)));
             return NULL;
         }
         num_allocated_blocks++;
@@ -95,8 +97,10 @@ void* smalloc(size_t size)
     {
         curr_node->is_free = false;
         num_free_blocks--;
-        num_free_bytes-= (curr_node->size - sizeof(MallocMetaData));
-        return curr_node+sizeof(MallocMetaData);
+        num_free_bytes-= (curr_node->total_size - sizeof(MallocMetaData));
+        // std::cout << "sizeof MetaData= " << sizeof(MallocMetaData) << std::endl;
+        // std::cout << "sum of sizes= " << curr_node+sizeof(MallocMetaData) << std::endl;
+        return static_cast<unsigned char*>((void*)(curr_node)) + sizeof(MallocMetaData);
     }
     return NULL;
 }
@@ -115,22 +119,32 @@ void* scalloc(size_t num, size_t size)
 
 void sfree(void* p)
 {
-    MallocMetaData* curr_meta = (MallocMetaData*)(p) - sizeof(MallocMetaData);
+    if(p==NULL)
+        return;
+    MallocMetaData* curr_meta = (MallocMetaData*)(static_cast<unsigned char*>(p) - sizeof(MallocMetaData));
+    // std::cout << "total_size of freed block= " << curr_meta->total_size << std::endl;
+    if(curr_meta->is_free)
+        return;
     num_free_blocks++;
-    num_free_bytes += (curr_meta->size - sizeof(MallocMetaData));
+    num_free_bytes += (curr_meta->total_size - sizeof(MallocMetaData));
     curr_meta->is_free = true;
 }
 
 void* srealloc(void* oldp, size_t size)
 {
+    if (size == 0)
+    {
+        return NULL;
+    }
     if (oldp == NULL)
     {
         return smalloc(size);
     }
 
-    MallocMetaData* curr_meta = (MallocMetaData*)(oldp) - sizeof(MallocMetaData);
-    size_t old_size = curr_meta->size;
-    if(size <= curr_meta->size)
+    size_t total_size = size + sizeof(MallocMetaData);
+    MallocMetaData* curr_meta = (MallocMetaData*)(static_cast<unsigned char*>(oldp) - sizeof(MallocMetaData));
+    size_t old_size = curr_meta->total_size -sizeof(MallocMetaData);
+    if(total_size <= curr_meta->total_size)
     {
         return oldp;
     }
@@ -147,6 +161,8 @@ void* srealloc(void* oldp, size_t size)
 
 size_t _num_free_blocks()
 {
+    return num_free_blocks;
+    /*
     size_t free_blocks_count = 0;
     MallocMetaData* curr_node = first_node;
     while(curr_node != NULL)
@@ -156,19 +172,23 @@ size_t _num_free_blocks()
         curr_node = curr_node->next;
     }
     return free_blocks_count;
+    */
 }
 
 size_t _num_free_bytes()
 {
+    return num_free_bytes;
+    /*
     size_t free_blocks_size = 0;
     MallocMetaData* curr_node = first_node;
     while(curr_node != NULL)
     {
         if(curr_node->is_free)
-            free_blocks_size+= (curr_node->size - sizeof(MallocMetaData));
+            free_blocks_size+= (curr_node->total_size - sizeof(MallocMetaData));
         curr_node = curr_node->next;
     }
     return free_blocks_size;
+    */
 }
 
 size_t _num_allocated_blocks()
@@ -190,3 +210,21 @@ size_t _size_meta_data()
 {
     return sizeof(MallocMetaData);
 }
+
+/*
+int main()
+{
+    void* first_block = smalloc(10);
+    void* second_block = smalloc(20);
+    sfree(first_block);
+    void* third_block = smalloc(7);
+    void* fourth_block = smalloc(15);
+    sfree(third_block);
+    std::cout << "allocated blocks = " << _num_allocated_blocks() << std::endl;
+    std::cout << "allocated bytes = " << _num_allocated_bytes() << std::endl;
+    std::cout << "freed blocks = " << _num_free_blocks() << std::endl;
+    std::cout << "freed bytes = " << _num_free_bytes() << std::endl;
+}
+*/
+
+
