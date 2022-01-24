64b7f341490993907c216730a8b3d7005a1caf10
muradek <muradek@campus.technion.ac.il>
2022-01-24 19:00:21 +0000

    malloc_2.h

---
+++ malloc_2.h
@@ -0,0 +1,20 @@
+//
+// Created by student on 1/22/22.
+//
+
+#ifndef WET4_MALLOC_2_H
+#define WET4_MALLOC_2_H
+
+struct MallocMetaData;
+void* smalloc(size_t size);
+void* scalloc(size_t num, size_t size);
+void sfree(void* p);
+void* srealloc(void* oldp, size_t size);
+size_t _num_free_blocks();
+size_t _num_free_bytes();
+size_t _num_allocated_blocks();
+size_t _num_allocated_bytes();
+size_t _num_meta_data_bytes();
+size_t _size_meta_data();
+
+#endif //WET4_MALLOC_2_H
